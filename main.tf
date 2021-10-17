ff47d3141427868c541b898571fdd2523e563e5e
mirkenstein <mirkenstein@gmail.com>
2021-10-16 23:37:58 -0500

    Added Route 53 record

--- main.tf
+++ main.tf
@@ -25,43 +25,42 @@ provider "aws" {
 #  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
 #}
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-  associate_public_ip_address	= true
-  for_each = toset(["es01", "es02", "es03"])
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = "~> 3.0"
+  associate_public_ip_address = true
+  for_each                    = toset(["es01", "es02", "es03"])
 
-  name = "tf-elk-node-${each.key}"
-
+  name                   = "tf-elk-node-${each.key}"
   ami                    = var.ami_image
   instance_type          = var.instance_type
-#  key_name               = "user1"
+  #  key_name               = "user1"
   monitoring             = true
-  vpc_security_group_ids =  [aws_security_group.allow_tls.id]
-  subnet_id              =  aws_subnet.tf_main.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id              = aws_subnet.tf_main.id
 
-  tags = {
+  tags      = {
     Terraform   = "true"
     Environment = "dev"
   }
-   user_data = "${file("user-data-apache.sh")}"
+  user_data = "${file("user-data-apache.sh")}"
 
 }
 
 
-
 resource "aws_route53_zone" "example" {
   name = "test.dfh.ai"
-    vpc {
+  vpc {
     vpc_id = aws_vpc.tf_main.id
   }
 }
 
 
-#resource "aws_route53_record" "www" {
-#  zone_id = aws_route53_zone.example.zone_id
-#  name    = "es1"
-#  type    = "A"
-#  ttl     = "300"
-#  records = [aws_instance.app_server.private_ip]
-#}
-#
+resource "aws_route53_record" "www" {
+  zone_id  = aws_route53_zone.example.zone_id
+  for_each = toset(["es01", "es02", "es03"])
+  name     = "${each.key}"
+  type     = "A"
+  ttl      = "300"
+  records  = [module.ec2_instance[each.key].private_ip]
+}
+
